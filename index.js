f8fb1fffc8cc60819708c31f23092401ae179054
Manish Goyal <manishgoyalbsr7@gmail.com>
2024-07-20 18:13:50 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import {referralMiddleware} from './middlewares.js';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 app.use(cors());
 app.use(express.json());
 
